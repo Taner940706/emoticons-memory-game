99ae09d2f55cc6091781215795db86056637fbc0
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-05-24 16:32:55 +0300

    flip the card func

--- memory-game/src/components/Card.js
+++ memory-game/src/components/Card.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-export default function Card({card, handleChoice}) {
+export default function Card({card, handleChoice, flipped}) {
   const handleClick = () => {
     handleChoice(card)
   }
   return (
     <div>
           <div className="card">
-            <div>
+            <div className={flipped ? "flipped": ""}>
               <img className="front" src={card.src} alt="front image" />
               <img className="back" src="/images/cover.jpg" alt="cover image" onClick={handleClick}/>
             </div>
